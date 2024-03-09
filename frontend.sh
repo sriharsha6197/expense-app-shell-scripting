60cbd68eaf2262f5cc32c13e79f7b68253375141
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-03-09 20:54:30 +0530

    setting up frontend server

--- frontend.sh
+++ frontend.sh
@@ -6,8 +6,7 @@ systemctl start nginx
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+unzip -d /tmp/frontend.zip /usr/share/nginx/html
 
 cp expense.conf /etc/nginx/default.d/expense.conf
 
