7c325092b9b83d8808d752080638b601860e100c
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-03-10 21:47:50 +0530

    logs-frontend

--- frontend.sh
+++ frontend.sh
@@ -1,13 +1,13 @@
-dnf install nginx -y
+dnf install nginx -y >> /tmp/expense.log
 
-systemctl enable nginx
-systemctl start nginx
+systemctl enable nginx  >> /tmp/access.log
+systemctl start nginx  >> /tmp/access.log
 
-rm -rf /usr/share/nginx/html/*
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+rm -rf /usr/share/nginx/html/*  >> /tmp/access.log
 
-unzip -d /usr/share/nginx/html /tmp/frontend.zip 
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip >> /tmp/access.log
+unzip -d /usr/share/nginx/html /tmp/frontend.zip >> /tmp/access.log 
 
-cp expense.conf /etc/nginx/default.d/expense.conf
+cp expense.conf /etc/nginx/default.d/expense.conf >> /tmp/access.log
 
-systemctl restart nginx
+systemctl restart nginx >> /tmp/access.log
