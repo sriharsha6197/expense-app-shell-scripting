e734ba976397ef70f86c356d455cc6eedfb428a6
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-03-11 14:03:30 +0530

    frontend-nfr

--- common.sh
+++ common.sh
@@ -3,9 +3,19 @@ expense_log=/tmp/expense.log
 download_extract(){
 echo download content
 curl -s -o /tmp/$component.zip https://expense-artifacts.s3.amazonaws.com/$component.zip >> $expense_log
-echo $?
+if [ $? -eq 0 ];
+then
+echo -e "\e[32mSUCCESS\e[0m"
+else
+echo -e "\e[31mFAILURE\e[0m"
+fi
 
 echo extract code
 unzip -d /usr/share/nginx/html /tmp/$component.zip >> $expense_log
-echo $?
+if [ $? -eq 0 ];
+then
+echo -e "\e[32mSUCCESS\e[0m"
+else
+echo -e "\e[31mFAILURE\e[0m"
+fi
 }
--- frontend.sh
+++ frontend.sh
@@ -24,7 +24,12 @@ download_extract
 
 echo copy content into expense configuration
 cp expense.conf /etc/nginx/default.d/expense.conf >> $expense_log
-echo $?
+if [ $? -eq 0 ];
+then
+echo -e "\e[32mSUCCESS\e[0m"
+else
+echo -e "\e[31mFAILURE\e[0m"
+fi
 
 echo enabling and restarting nginx
 systemctl enable nginx  >> $expense_log
