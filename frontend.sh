78fdd811bd26be693679a5b93c3918f3c6c45ac6
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-03-11 13:53:18 +0530

    frontend-nfr

--- frontend.sh
+++ frontend.sh
@@ -3,7 +3,7 @@ component=frontend
 
 echo Installing nginx
 dnf install nginx -y >> $expense_log
-if[$? -eq 0]; then
+if[$?-eq0]; then
 echo -e "\e[32mSUCCESS\e[0m"
 else
 echo -e "\e[31mFAILURE\e[0m"
