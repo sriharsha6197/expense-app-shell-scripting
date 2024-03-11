25daf3a9039d9b19209eb38e97c8bc3fc2dbb58d
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-03-11 18:31:50 +0530

    mysql-nfr

--- mysql.sh
+++ mysql.sh
@@ -1,10 +1,20 @@
+echo disabling module mysql
 dnf module disable mysql -y
+echo $?
 
+echo copying mysql repo into repo file
 cp mysql.repo /etc/yum.repos.d/mysql.repo
+echo $?
 
+echo Installing mysql
 dnf install mysql-community-server -y
+echo $?
 
+echo enabling and restarting mysql
 systemctl enable mysqld
 systemctl start mysqld
+echo $?
 
+echo mysql installation
 mysql_secure_installation --set-root-pass Sriharsha@1
+echo $?
