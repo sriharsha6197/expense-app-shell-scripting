cf6f50801565704d708c73e81ef6d9c9d3dcd549
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-03-11 14:02:16 +0530

    frontend-nfr

--- frontend.sh
+++ frontend.sh
@@ -12,7 +12,12 @@ fi
 
 echo removing old content
 rm -rf /usr/share/nginx/html/*  >> $expense_log
-echo $?
+if [ $? -eq 0 ];
+then
+echo -e "\e[32mSUCCESS\e[0m"
+else
+echo -e "\e[31mFAILURE\e[0m"
+fi
 
 echo download and extract
 download_extract
